f41ddf87215acaf72860e8896f8778103cf6a05b
Mada <mada.elenagheorghe@gmail.com>
2020-03-14 12:23:01 +0200

    added TestGit class

---
+++ Assignment/src/Classes/TestGit.java
@@ -0,0 +1,7 @@
+package Classes;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git! The name of the license project is Awareness of crowded places using a mobile application ");
+    }
+}
